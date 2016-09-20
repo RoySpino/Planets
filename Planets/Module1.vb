5a9e88d6df635894be9cb30d7817474f6c615c83
Roy Banuelos <spinosaurus001@hotmail.com>
2016-09-19 22:26:27 -0700

    inital git dump

---
+++ Planets/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ren As Render
+        ren = New Render(640, 480)
+
+        ren.X = 100
+        ren.Y = 100
+        ren.Run()
+    End Sub
+
+End Module
---
+++ Planets/Render.vb
@@ -0,0 +1,29 @@
+﻿Imports OpenTK.Graphics.OpenGL
+Imports OpenTK
+Imports System
+Imports System.Drawing
+
+Public Class Render
+    Inherits GameWindow
+
+    Public Sub New(x As Integer, y As Integer)
+        MyBase.New(x, y)
+    End Sub
+
+    Protected Overrides Sub OnLoad(e As EventArgs)
+        MyBase.OnLoad(e)
+    End Sub
+
+    Protected Overrides Sub OnUpdateFrame(e As FrameEventArgs)
+        MyBase.OnUpdateFrame(e)
+    End Sub
+
+    Protected Overrides Sub OnResize(e As EventArgs)
+        MyBase.OnResize(e)
+    End Sub
+
+    Protected Overrides Sub OnRenderFrame(e As FrameEventArgs)
+        MyBase.OnRenderFrame(e)
+    End Sub
+
+End Class
