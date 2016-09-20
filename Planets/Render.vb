ea84c1a370772bbb8a252f1c7a7cf556306f60c5
Roy Banuelos <spinosaurus001@hotmail.com>
2016-09-19 23:03:30 -0700

    added 3D GL init function

--- Planets/Render.vb
+++ Planets/Render.vb
@@ -5,9 +5,18 @@ Imports System.Drawing
 
 Public Class Render
     Inherits GameWindow
+    Dim xres As Double
+    Dim yres As Double
+    Dim mat_spec = New Double() {1.0, 1.0, 1.0, 1.0}
+    Dim mat_shini = New Double() {50.0}
+    Dim ambiant = New Double() {0.1484, 0.0, 0.5781}
+    Dim light_Pos = New Double() {1, 1, 1, .0}
+
 
     Public Sub New(x As Integer, y As Integer)
         MyBase.New(x, y)
+        xres = x
+        yres = y
     End Sub
 
     Protected Overrides Sub OnLoad(e As EventArgs)
@@ -24,6 +33,35 @@ Public Class Render
 
     Protected Overrides Sub OnRenderFrame(e As FrameEventArgs)
         MyBase.OnRenderFrame(e)
+        init()
     End Sub
 
+
+    Private Sub init()
+        GL.Viewport(0, 0, xres, yres)
+        GL.MatrixMode(MatrixMode.Projection)
+        GL.LoadIdentity()
+        GL.MatrixMode(MatrixMode.Modelview)
+        GL.LoadIdentity()
+
+        GL.Enable(EnableCap.Texture2D)
+        GL.Enable(EnableCap.Blend)
+        GL.ClearColor(Color.Black)
+        GL.ClearDepth(1.0)
+        GL.DepthFunc(DepthFunction.Lequal)
+        GL.ShadeModel(ShadingModel.Smooth)
+        GL.MatrixMode(MatrixMode.Projection)
+        GL.LoadIdentity()
+
+        Matrix4.CreatePerspectiveFieldOfView(45.0, xres / yres, 0.1, 900.0)
+        GL.Material(MaterialFace.Front, MaterialParameter.Specular, mat_spec)
+        GL.Material(MaterialFace.Front, MaterialParameter.Shininess, mat_shini)
+
+        GL.Light(LightName.Light0, LightParameter.Position, light_Pos)
+        GL.Light(LightName.Light0, LightParameter.Ambient, ambiant)
+
+        GL.MatrixMode(MatrixMode.Modelview)
+        GL.Enable(EnableCap.Lighting)
+        GL.Enable(EnableCap.Light0)
+    End Sub
 End Class
