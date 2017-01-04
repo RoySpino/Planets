0c897510203f1b132df3350e2ad10a36c5d884ea
RoySpino <spinosaurus001@hotmail.com>
2017-01-03 23:11:34 -0800

    Update sphere.vb

--- Planets/sphere.vb
+++ Planets/sphere.vb
@@ -1,101 +1,148 @@
-﻿Imports OpenTK.Graphics.OpenGL
 Imports OpenTK
+Imports OpenTK.Graphics.OpenGL
 Imports System
-Imports System.Drawing
+Imports System.Runtime.InteropServices
 
+Public Class SolidSphere
+    Dim X, Y, Z, rad, gblRings, gblSectors As Double
+    Dim rotx, roty, rotz As Double
+    Dim vl, nl, tl As List(Of Double)
+    Dim il As List(Of Integer)
+    Dim vert() As Double
+    Dim normal() As Double
+    Dim texcoords() As Double
+    Dim indices() As Integer
 
-Public Class sphere
-    Dim rad As Double
-    Dim vert As List(Of Double) = New List(Of Double)()
-    Dim normal As List(Of Double) = New List(Of Double)()
-    Dim texcoords As List(Of Double) = New List(Of Double)()
-    Dim indices As List(Of Integer) = New List(Of Integer)()
-    Dim rx, ry, rz As Double
-    Dim lx, ly, lz As Double
-
-    Public Sub New()
-        redraw(1, 16, 32)
+    Sub New()
+        redraw(1, 15, 32)
     End Sub
 
-    Public Sub New(radius As Double, rings As Integer, sectors As Integer)
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub New(radius As Double, rings As Double, sectors As Double)
         redraw(radius, rings, sectors)
     End Sub
 
-    Private Sub redraw(radius As Double, rings_ As Integer, sectors_ As Integer)
-        rad = radius
-        Dim RINGS, SECTORS As Integer
-        Dim r, s, y, x, z As Double
-        r = 1.0 / (RINGS - 1.0)
-        s = 1.0 / (SECTORS - 1.0)
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub redraw(radius As Double, rings As Double, sectors As Double)
+        Dim R, S As Double
+        Dim x, y, z As Double
+        rotx = roty = rotz = 0                ' Default rotation For sphears
+        gblRings = rings
+        gblSectors = sectors
+        R = 1.0 / CDbl(rings - 1)
+        S = 1.0 / CDbl(sectors - 1)
 
-        vert.Clear()
-        normal.Clear()
-        texcoords.Clear()
+        vl = New List(Of Double)()
+        nl = New List(Of Double)()
+        tl = New List(Of Double)()
+        il = New List(Of Integer)()
 
-        RINGS = rings_
-        SECTORS = sectors_
+        For i As Integer = 0 To (rings - 1)
+            For u As Integer = 0 To (sectors - 1)
+                x = Math.Sin(-MathHelper.PiOver2 + MathHelper.Pi * CDbl(i) * R)
+                y = Math.Cos(MathHelper.TwoPi * CDbl(u) * S) * Math.Sin(MathHelper.Pi * CDbl(i) * R)
+                z = Math.Sin(MathHelper.TwoPi * CDbl(u) * S) * Math.Sin(MathHelper.Pi * CDbl(i) * R)
 
+                tl.Add(u * S)
+                tl.Add(i * R)
 
-        For i As Integer = 0 To (RINGS - 1)
-            For u As Integer = 0 To (SECTORS - 1)
-                y = Math.Sin((Math.PI * 2) - (Math.PI * 2) + Math.PI * i * r)
-                x = Math.Cos(2 * Math.PI * u * s) * Math.Sin(Math.PI * i * r)
-                z = Math.Sin(2 * Math.PI * u * s) * Math.Sin(Math.PI * i * r)
-                texcoords.Add(u * s)
-                texcoords.Add(i * r)
+                vl.Add(x * radius)
+                vl.Add(y * radius)
+                vl.Add(z * radius)
 
-                vert.Add(x * radius)
-                vert.Add(y * radius)
-                vert.Add(z * radius)
-
-                normal.Add(x)
-                normal.Add(y)
-                normal.Add(z)
+                nl.Add(x)
+                nl.Add(y)
+                nl.Add(z)
             Next
         Next
 
-        indices.Clear()
-        For i As Integer = 0 To (RINGS - 2)
-            For u As Integer = 0 To (SECTORS - 1)
-                indices.Add(i * SECTORS + u)
-                indices.Add(i * SECTORS + (u + 1))
-                indices.Add((i + 1) * SECTORS + (u + 1))
-                indices.Add((i + 1) * SECTORS + u)
+        For i As Integer = 0 To (rings - 2)
+            For u As Integer = 0 To (sectors - 2)
+                il.Add(i * sectors + u)
+                il.Add(i * sectors + (u + 1))
+                il.Add((i + 1) * sectors + (u + 1))
+                il.Add((i + 1) * sectors + u)
             Next
         Next
+
+        texcoords = tl.ToArray()
+        vert = vl.ToArray()
+        normal = nl.ToArray()
+        indices = il.ToArray()
+
+        vl.Clear()
+        nl.Clear()
+        tl.Clear()
+        il.Clear()
     End Sub
 
-    Public Sub draw()
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub draw(x As Double, y As Double, z As Double)
         GL.MatrixMode(MatrixMode.Modelview)
+
         GL.PushMatrix()
 
         ' change position
-        GL.Translate(lx, ly, lz)
+        GL.Translate(x, y, z)
+        Me.X = x
+        Me.Y = y
+        Me.Z = z
 
-        GL.Rotate(rx, 1, 0, 0)
-        GL.Rotate(ry, 0, 1, 0)
-        GL.Rotate(rz, 0, 0, 1)
+        GL.Rotate(rotx, 1, 0, 0)
+        GL.Rotate(roty, 0, 1, 0)
+        GL.Rotate(rotz, 0, 0, 1)
 
         GL.EnableClientState(ArrayCap.VertexArray)
         GL.EnableClientState(ArrayCap.NormalArray)
         GL.EnableClientState(ArrayCap.TextureCoordArray)
 
-        GL.VertexPointer(3, VertexPointerType.Double, 0, vert.ToArray())
-        GL.NormalPointer(NormalPointerType.Double, 0, normal.ToArray())
-        GL.TexCoordPointer(2, TexCoordPointerType.Double, 0, texcoords.ToArray())
-        GL.DrawElements(BeginMode.Triangles, indices.Count, DrawElementsType.UnsignedInt, indices.ToArray())
+        GL.VertexPointer(3, VertexPointerType.Double, 0, vert)
+        GL.NormalPointer(NormalPointerType.Double, 0, normal)
+        GL.TexCoordPointer(2, TexCoordPointerType.Double, 0, texcoords)
+        GL.DrawElements(PrimitiveType.Quads, indices.Length, DrawElementsType.UnsignedInt, indices)
+
+        GL.DisableClientState(ArrayCap.VertexArray)
+        GL.DisableClientState(ArrayCap.NormalArray)
+        GL.DisableClientState(ArrayCap.TextureCoordArray)
         GL.PopMatrix()
     End Sub
 
-    Public Sub rot(x As Double, y As Double, z As Double)
-        rx = x
-        ry = y
-        rz = z
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub rot(x As Double, y As Double, z As Double)
+        rotx = x
+        roty = y
+        rotz = z
     End Sub
 
-    Public Sub loc(x As Double, y As Double, z As Double)
-        lx = x
-        ly = y
-        lz = z
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub loc(x As Double, y As Double, z As Double)
+        Me.X = x
+        Me.Y = y
+        Me.Z = z
+    End Sub
+
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Function getX() As Double
+        Return X
+    End Function
+
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Function getY() As Double
+        Return Y
+    End Function
+
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Function getZ() As Double
+        Return Z
+    End Function
+
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Function getRad() As Double
+        Return rad
+    End Function
+
+    ' //////////////////////////////////////////////////////////////////////////////////////
+    Sub resize(nrad As Double)
+        redraw(nrad, gblRings, gblSectors)
     End Sub
 End Class
